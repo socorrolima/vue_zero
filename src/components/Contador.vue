0b1c92f87dd1d194437b7c86c93614eaf3bfdbb5
Socorro Lima <socorrolima41@gmail.com>
2024-10-06 18:03:29 -0300

    contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,30 @@
+
+<script setup>
+
+import {ref} from 'vue';
+
+const Contador = ref(0);
+
+const incrementar = () =>{
+  Contador.value++;
+}
+
+const decrementar = () =>{
+  Contador.value--;
+}
+
+</script>
+
+
+<template>
+
+  {{ Contador }}<br><br>
+  <button @click="incrementar">Incrementar</button><br><br>
+  <button @click="decrementar">Decrementar</button>
+
+</template>
+
+
+<style scoped>
+
+</style>
